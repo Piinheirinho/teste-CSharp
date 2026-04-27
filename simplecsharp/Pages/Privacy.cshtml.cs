63b4bdc4b8b10e10a31e9f2984644ff27a78cf0d
R4fuel <a14715@oficina.pt>
2026-04-27 10:17:05 +0000

    commir

---
+++ simplecsharp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace simplecsharp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+   ViewData["TimeStamp"] = dateTime;
+    }
+}
+
